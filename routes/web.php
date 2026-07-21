659382a41eda9a151a1075f5cea03cf7ac992aa0
Reza Asian <rezasihombikng34@gmail.com>
2026-07-21 08:51:22 +0700

    Modul 2 - Rute dan Controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GreetingsController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Rute dasar dihubungkan ke method welcome()
+Route::get('/', [GreetingsController::class, 'welcome']);
+
+// Rute dengan parameter dihubungkan ke method greet()
+Route::get('/greet/{name}/{npm}', [GreetingsController::class, 'greet']);
